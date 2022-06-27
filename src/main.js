654dc0f6097ba8b240224f0faf4b7ad76c27f73c
LeylaSolange <leylagamarra.movistar@gmail.com>
2022-06-27 13:25:02 -0500

    avance json

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import { example } from './data.js';
 
-console.log(example, data);
+//import data from './data/pokemon/pokemon.js';
+
+let containerPokemon = document.getElementById("containerPokemon");
+
+let pokemon =fetch("./data/pokemon/pokemon.json")
+.then(response => {
+   return response.json();
+})
+.then(pokemon => console.log(pokemon));
+ 
+let showPokemon = async()=>{
+ for(let i =1; i<=pokemonNumber; i++){
+    await pokemon(i); 
+}
+}
