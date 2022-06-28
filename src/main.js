1a4114779dd3990cb5031692195ddc42255df20b
LeylaSolange <leylagamarra.movistar@gmail.com>
2022-06-28 13:05:05 -0500

    crear tarjetas con la data

--- src/main.js
+++ src/main.js
@@ -1,17 +1,53 @@
 //import { example } from './data.js';
 
-//import data from './data/pokemon/pokemon.js';
+import data from './data/pokemon/pokemon.js';
 
 let containerPokemon = document.getElementById("containerPokemon");
 
-let pokemon =fetch("./data/pokemon/pokemon.json")
-.then(response => {
-   return response.json();
-})
-.then(pokemon => console.log(pokemon));
+// creartePokemon(data.pokemon[0]);  (referencia)
+// let pokemon =fetch("./data/pokemon/pokemon.json")
+// .then(response => {
+//    return response.json();
+// })
+// .then(pokemon => console.log(pokemon.pokemon[0]));
  
-let showPokemon = async()=>{
- for(let i =1; i<=pokemonNumber; i++){
-    await pokemon(i); 
+// let showPokemon = async()=>{
+//  for(let i =1; i<=pokemonNumber; i++){
+//     await pokemon(i); 
+// }
+
+for(let i=0; i< data.pokemon.length; i++ ){
+   createPokeCard(data.pokemon[i]);
 }
+// function createPokemon(pokemon){
+//    console.log(pokemon.name)
+
+// }
+
+function createPokeCard (pokemon){
+   let card=document.createElement("div");
+   card.classList.add("card-pokemon");
+
+   let imgContent = document.createElement("div");
+   imgContent.classList.add("img-content");
+
+   let imgPokemon = document.createElement("img");
+   imgPokemon.src = pokemon.img;
+   
+ 
+   imgContent.appendChild(imgPokemon);
+
+   let number = document.createElement("p");
+   number.textContent = pokemon.num;
+
+   let name = document.createElement("p");
+   name.classList.add("namePokemon");
+   name.textContent = pokemon.name
+
+   card.appendChild(imgContent);
+   card.appendChild(number);
+   card.appendChild(name);
+
+   containerPokemon.appendChild(card);
+   
 }
